9fb9deb0f4706b25e902a1738cef741db7052b4d
Andrew Jones <princeajsa@gmail.com>
2024-10-20 18:55:41 +0200

    Toe TacTic by Andrew

--- src/tic/tac/util/SomeExceptionException.java
+++ src/tic/tac/util/SomeExceptionException.java
@@ -2,19 +2,7 @@ package tic.tac.util;
 
 public class SomeExceptionException extends UnsupportedOperationException {
 
-    public SomeExceptionException() {
-        super();
-    }
-
     public SomeExceptionException(String message) {
         super(message);
-    }
-
-    public SomeExceptionException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public SomeExceptionException(Throwable cause) {
-        super(cause);
     }
 }
